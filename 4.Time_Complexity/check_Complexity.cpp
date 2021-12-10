e2e524240c2682a82f2f55d97ef178c554e3db39
Eureka-Viraj <viraj.deshpande19@vit.edu>
2021-12-10 22:10:54 +0530

    the documentation part of timecomplexity

---
+++ 4.Time_Complexity/check_Complexity.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+
+int main()
+{
+    int n;
+    cin >> n;
+    
+    return 0;
+}
+/*
+You must need to understand the purpose of this timecomplexity and spacecomplexity to 
+reach out the ultimate solution! 
+*/
