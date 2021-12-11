0d072ab478c1243dfb8f22c4d37215c9dc3f19bd
Eureka-Viraj <viraj.deshpande19@vit.edu>
2021-12-11 16:28:23 +0530

    Just added the corretion of Accending order

--- 5.Arrays/4_Selection-Sort.cpp
+++ 5.Arrays/4_Selection-Sort.cpp
@@ -11,7 +11,7 @@ int main()
     }
     for(int i=0; i<n-1; i++){
         for(int j=i+1;j<n; j++){
-            if(arr[j]>arr[i]){
+            if(arr[i]>arr[j]){
                 int temp =arr[j];
                 arr[j] = arr[i];
                 arr[i] = temp;
